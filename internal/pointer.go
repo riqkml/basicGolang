72707ce9e38940004911a342d345e781b4115f92
riqkmlid <riqki.amrela@salt.co.id>
2023-03-12 14:05:42 +0700

    learn: pointer & new

---
+++ internal/pointer.go
@@ -0,0 +1,26 @@
+package internal
+
+import "log"
+
+type Address struct {
+	City     string
+	Province string
+	Country  string
+}
+
+func Pointer() {
+	address1 := Address{"Bekasi", "Jawa Barat", "Indonesia"}
+	address2 := &address1
+
+	address1.City = "Purwakarta"
+
+	address3 := new(Address)
+	address3 = &address1
+	address3.City = "Malang"
+	address3.Province = "Jawa Timur"
+	address3.Country = "Indonesia"
+
+	log.Println("address1 ", address1)
+	log.Println("address2 ", address2)
+	log.Println("address3 ", address3)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "examplego/internal"
+
+func main() {
+	internal.Pointer()
+}
